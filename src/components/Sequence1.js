9136e208b6accad0e853284b3ceecdaa5ff759e7
Suman Sourabh <sumsourabh14@gmail.com>
2022-09-19 17:55:06 +0530

    add more updates

--- src/components/Sequence1.js
+++ src/components/Sequence1.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { sequence1 } from '../story/sequence1Story';
+import { styles } from '../styles';
+import ChoiceBtn from './ChoiceBtn';
 
 function Sequence1(props) {
   return (
-    <div>
-        <p>{sequence1.story}</p>
+    <div className={`${styles.flexBoxCol} border-2 h-screen`}>
+        {sequence1.story.map(paragraph => {
+          return (
+            <p className='mb-2'>{paragraph.p}</p>
+          )
+        })}
         <h2>{sequence1.question}</h2>
-        <div>
-          <button onClick={props.onFirstChoiceClick}>{sequence1.choices[0].choice1}</button>
-          <p>{sequence1.choices[0].choice1body}</p>
-        </div>
-        <div>
-          <button onClick={props.onSecondChoiceClick}>{sequence1.choices[1].choice2}</button>
-          <p>{sequence1.choices[1].choice2body}</p>
+
+        <div className={`${styles.flexBox} flex-col md:flex-row mt-24 border-2`}>
+          <div className={`${styles.flexBoxCol} ${styles.choiceBox}`}>
+            <ChoiceBtn onChoiceClick={props.onFirstChoiceClick} choiceText={sequence1.choices[0].choice1} />
+            <p className={`${styles.choiceParaPadding}`}>{sequence1.choices[0].choice1body}</p>
+          </div>
+          <div className={`${styles.flexBoxCol} ${styles.choiceBox} mt-14 md:mt-0 md:ml-14`}>
+            <ChoiceBtn onChoiceClick={props.onSecondChoiceClick} choiceText={sequence1.choices[1].choice2} />
+            <p className={`${styles.choiceParaPadding}`}>{sequence1.choices[1].choice2body}</p>
+          </div>
         </div>
     </div>
   )
--- src/styles.js
+++ src/styles.js
@@ -1,9 +1,14 @@
 export const styles = {
     flexBox: "flex justify-center items-center",
+    flexBoxCol: "flex justify-center items-center flex-col",
 
     mainMenuOptions: "hover:scale-[1.02] duration-300 opacity-50 hover:opacity-100 shadow-2xl",
 
-    mainMenuOptionsTitle: "absolute bottom-2 right-2 lg:text-xl",
+    mainMenuOptionsTitle: "absolute bottom-2 right-2 lg:text-xl text-white",
 
     mainMenuIcons: "text-4xl lg:text-6xl",
+
+    choiceBox: "bg-stone-100 pb-2 shadow-lg",
+
+    choiceParaPadding: "px-6",
 }
