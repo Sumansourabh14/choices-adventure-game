a9a60ec6284c5d492eff7c2aa14331a3e53c9658
Suman Sourabh <sumsourabh14@gmail.com>
2022-09-20 23:46:56 +0530

    add a bit of prologue story

--- src/story/prologueStory.js
+++ src/story/prologueStory.js
@@ -2,6 +2,83 @@ export const prologueStory = {
     startPage: [
         {
             id: 1,
-        }
+            text: "Outside Hillside Hotel, London, 2005",
+        },
+        {
+            id: 2,
+            text: "Heavy rain. Occasional lightning strikes and not a lot of activity in the street.",
+        },
+        {
+            id: 3,
+            text: "Outside the shiny Hillside hotel, a man waits under a shed across the street, dressed in all black. He is checking his watch, and looking for … something.",
+        },
+        {
+            id: 4,
+            text: "His phone rings.",
+        },
+        {
+            id: 5,
+            text: "Beep, beep.",
+        },
+        {
+            id: 6,
+            text: "Eyes fixed at the front gate of the hotel, he picks his phone from his pocket.",
+        },
+    ],
+    secondPage: [
+        {
+            id: 1,
+            text: "Luther: Keep your eyes on the target, Travis. We don't want to be in a situation to lose that guy. The higher authorities won't approve."
+        },
+        {
+            id: 2,
+            text: "Travis: I know. Eyes are on him."
+        },
+        {
+            id: 3,
+            text: "Luther: He would be coming any minute now. Get ready to run."
+        },
+        {
+            id: 4,
+            text: "Travis: Roger."
+        },
+        {
+            id: 5,
+            text: "Luther: And one more thing! Don't attract attention."
+        },
+        {
+            id: 6,
+            text: "Travis lets out a half smile."
+        },
+        {
+            id: 7,
+            text: "Travis: All right."
+        },
+        {
+            id: 8,
+            text: "He hangs up the phone and quickly puts it in his side pocket."
+        },
+        {
+            id: 9,
+            text: "By the time he was talking, the rain had already subsided and activity had resumed in the street. This only confirmed one thing."
+        },
+        {
+            id: 10,
+            text: "Travis’ mission is not going to be easy."
+        },
+        {
+            id: 11,
+            text: "Suddenly, a screeching sound is heard somewhere from the right, far from the Hillside Hotel."
+        },
+    ],
+    choices: [
+        {
+            choice1: "Distracted",
+            choice1body: "Look at the area where the noise originated.",
+        },
+        {
+            choice2: "Focused",
+            choice2body: "Keep eyes fixed at the front door of the hotel.",
+        },
     ]
 }
--- src/story/sequence1Story.js
+++ src/story/sequence1Story.js
@@ -11,11 +11,37 @@ export const sequence1 = {
     choices: [
         {
             choice1: "Positive",
-            choice1body: "I am okay!"
+            choice1body: "I am okay!",
         },
         {
             choice2: "Negative",
-            choice2body: "I don't feel so good."
+            choice2body: "I don't feel so good.",
         },
     ]
 }
+
+// export const sequence1 = {
+//     story: [
+//         {
+//             p: "This is a game paragraph 1.",
+//         },
+//         {
+//             p: "This is a game paragraph 2.",
+//         },
+//     ],
+//     question: "What do you say?",
+//     choices: [
+//         {
+//             id: 1,
+//             choiceTitle: "Positive",
+//             choicebody: "I am okay!",
+//             destination: "onFirstChoiceClick",
+//         },
+//         {
+//             id: 2,
+//             choiceTitle: "Negative",
+//             choicebody: "I don't feel so good.",
+//             destination: "onSecondChoiceClick",
+//         },
+//     ]
+// }
